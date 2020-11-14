5d262e84ef657a9ef8f046839a3568a5ca5c4cb3
yamenosta <ostayamen@hotmail.com>
2020-11-14 18:12:55 +0100

    first commit

---
+++ src/main/java/com/gitaction/GitActionApplication.java
@@ -0,0 +1,13 @@
+package com.gitaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitActionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitaction/GitActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
